276273e242d74aa3e2720370cbd8c0a2a35224ba
Iacopo Papalini <iacopo@papalini.net>
2019-11-03 10:53:11 +0100

    making run.sh script run in cron

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ pyenv activate spiaggiari
 echo "$(date) Start"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}" || exit
-python bin/fetch_votes.py "$@" 2>&1
+python "${DIR}/bin/fetch_votes.py" "$@" 2>&1
 echo "$(date) End"
