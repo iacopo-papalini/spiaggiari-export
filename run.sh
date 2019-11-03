aecc8681a03cffb35afce17582ce314f00372682
Iacopo Papalini <iacopo@papalini.net>
2019-11-03 10:44:25 +0100

    fixing run script

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@ eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 pyenv activate spiaggiari
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "${DIR}" || exit
 python bin/fetch_votes.py "$@"
